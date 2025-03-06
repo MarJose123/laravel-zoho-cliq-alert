e8d9f0a03bed184810fa8a345f4ccd80015cc28e
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-03-06 21:57:09 +0800

    ✨ feat(`SendToCliqJob`): Implement Zoho Cliq alert sending.  The job now uses the Zoho API to send messages to Cliq channels.

--- src/Jobs/SendToCliqJob.php
+++ src/Jobs/SendToCliqJob.php
@@ -5,19 +5,56 @@ namespace MarJose123\ZohoCliqAlert\Jobs;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+use MarJose123\ZohoCliqAlert\Concerns\Zoho;
+use MarJose123\ZohoCliqAlert\Exceptions\CliqAlertException;
+use MarJose123\ZohoCliqAlert\ZohoDataCenter;
 
 class SendToCliqJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Zoho;
 
     /**
      * The maximum number of unhandled exceptions to allow before failing.
      */
     public int $maxExceptions = 3;
 
-    public function __construct(public array $payload) {}
+    /**
+     * @param object{
+     *     channel: string,
+     *     bot: string,
+     *     to: string,
+     *     message: string,
+     *     embed: array,
+     *     dataCenter: ZohoDataCenter,
+     *     markAsRead: bool
+     * } $cliq
+     */
+    public function __construct(private readonly object $cliq) {}
 
-    public function handle(): void {}
+    /**
+     * @throws CliqAlertException
+     * @throws ConnectionException
+     */
+    public function handle(): void
+    {
+        $dataCenterOauthUrl = $this->cliq->dataCenter->getOauthBaseUrl();
+        $dataCenterBaseUrl = $this->cliq->dataCenter->getBaseUrl();
+        $endpoint = "{$dataCenterBaseUrl}/api/v2/channelsbyname/{$this->cliq->channel}/message";
+        if (! $this->cliq->bot) {
+            $endpoint .= "?bot_unique_name={$this->cliq->bot}";
+        }
+
+        $payload = ['text' => $this->cliq->message];
+        if (! empty($this->cliq->embed)) {
+            $payload = array_merge($payload, $this->cliq->embed);
+        }
+
+        Http::withToken($this->getAccessToken($dataCenterOauthUrl))
+            ->post($endpoint, $payload);
+    }
 }
