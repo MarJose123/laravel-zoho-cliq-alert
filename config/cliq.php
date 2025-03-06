cb9f1851f13f4addf87c410b6c31218bc01ae412
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-03-06 21:57:17 +0800

    ✨ feat(config): Add new `cliq.php` config file and remove old `zoho-cliq-alert.php`.

---
+++ config/cliq.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | OAuth Credentials
+    |--------------------------------------------------------------------------
+    |
+    | These are OAuth credentials required for authentication with Zoho Cliq API.
+    | You can obtain these credentials from your Zoho Cliq Developer Console:
+    | https://api-console.zoho.com/
+    |
+    */
+    'client_id' => env('ZOHO_CLIENT_ID', ''),
+    'client_secret' => env('ZOHO_CLIENT_SECRET', ''),
+
+];
